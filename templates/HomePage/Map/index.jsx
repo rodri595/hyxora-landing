630c2f743cebf8899ed4048b5b35166c7db0232c
Rodrigo Erazo <rodri.595@hotmail.com>
2026-07-06 23:12:05 +0200

    feat: update text in MapSection to clarify NFT Founders count and improve Spanish translation

--- templates/HomePage/Map/index.jsx
+++ templates/HomePage/Map/index.jsx
@@ -7,14 +7,14 @@ import chevronSVG from "@/assets/imgs/icons/chevron.svg";
 const MapSection = () => {
   return (
     <section className="flex w-full" id="map">
-      <div className="flex gap-[70px] items-start max-w-[1240px] w-full mx-auto max-xl:px-[32px] max-md:px-[16px] max-lg:flex-col max-lg:gap-[40px]">
+      <div className="flex gap-[40px] items-start max-w-[1340px] w-full mx-auto max-xl:px-[32px] max-md:px-[16px] max-lg:flex-col ">
         {/* Left — globe focused on the Americas */}
         <Globe />
 
         {/* Right — Content */}
-        <div className="flex flex-col gap-[80px] max-md:gap-[48px] items-start pt-[40px] max-md:pt-0 shrink-0 w-fullmax-lg:w-full">
+        <div className="flex flex-col gap-[80px] max-md:gap-[48px] items-start pt-[40px] max-md:pt-0 ">
           {/* Text */}
-          <div className="flex flex-col gap-[28px] items-start w-[471px]  max-lg:w-full">
+          <div className="flex flex-col gap-[28px] items-start max-w-[471px] w-full max-sm:gap-5">
             {/* Badge */}
             {/* Hot Badge */}
             <Link
@@ -52,18 +52,14 @@ const MapSection = () => {
                 className="relative w-[12px] h-[12px]"
               />
             </Link>
-            {/* <div className="relative flex items-center justify-center px-[14px] py-[10px] rounded-[32px] border-[0.7px] border-[rgba(25,54,63,0.02)] bg-[rgba(25,54,63,0.02)] shadow-[inset_0px_0px_4px_0px_rgba(25,54,63,0.04)]">
-              <p className="font-medium text-[12px] leading-[normal] text-[rgba(25,54,63,0.7)] tracking-[-0.48px] whitespace-nowrap">
-                Accede a la plataforma
-              </p>
-            </div> */}
+
             {/* Title */}
-            <p className="font-medium text-[40px] max-md:text-[28px] leading-[normal] tracking-[-1.6px] max-md:tracking-[-1.12px] w-[346px] max-md:w-full">
+            <p className="font-medium text-[40px] max-lg:text-[32px] max-md:text-[28px] max-sm:text-[24px] leading-[normal] tracking-[-1.6px] max-lg:tracking-[-1.28px] max-md:tracking-[-1.12px] max-sm:tracking-[-0.96px] w-[346px] max-md:w-full">
               <span className="text-[rgba(25,54,63,0.6)]">Hyxora </span>
               <span className="text-[#19363f]">en el mundo</span>
             </p>
             {/* Description */}
-            <p className="font-normal text-[16px] leading-[24px] text-[rgba(25,54,63,0.7)] tracking-[-0.32px] w-full">
+            <p className="font-normal text-[16px] max-md:text-[14px] max-sm:text-[13px] leading-6 max-sm:leading-5 text-[rgba(25,54,63,0.7)] tracking-[-0.32px] max-sm:tracking-[-0.26px] w-full">
               Estamos abriendo colaboraciones por todo el planeta. ¿Quieres
               formar parte de la expansión de Hyxora?{" "}
               <a
@@ -74,32 +70,31 @@ const MapSection = () => {
               </a>
             </p>
           </div>
-
           {/* Stats */}
-          <div className="flex gap-[30px] items-start  w-full max-md:gap-[20px] ">
-            <div className="flex flex-col gap-[16px] items-start ">
-              <p className="font-medium text-[28px] leading-[normal] text-[#19363f] tracking-[-1.36px]">
+          <div className="grid grid-cols-3 gap-7.5 items-start debug  max-xl:grid-cols-1 w-full">
+            <div className="flex flex-col gap-[16px] items-start max-sm:w-full">
+              <p className="font-medium text-[28px] max-lg:text-[24px] max-md:text-[22px] max-sm:text-[20px] leading-[normal] text-[#19363f] tracking-[-1.36px] max-sm:tracking-[-1px]">
                 100% Autocustodia
               </p>
-              <p className="font-normal text-[16px] leading-[24px] text-[rgba(25,54,63,0.7)] tracking-[-0.32px]">
+              <p className="font-normal text-[16px] max-md:text-[14px] max-sm:text-[13px] leading-6 max-sm:leading-5 text-[rgba(25,54,63,0.7)] tracking-[-0.32px] max-sm:tracking-[-0.26px]">
                 Sé el dueño de tus fondos
               </p>
             </div>
-            <div className="flex flex-col gap-[16px] items-start ">
-              <p className="font-medium text-[28px] leading-[normal] text-[#19363f] tracking-[-1.36px]">
+            <div className="flex flex-col gap-[16px] items-start max-sm:w-full">
+              <p className="font-medium text-[28px] max-lg:text-[24px] max-md:text-[22px] max-sm:text-[20px] leading-[normal] text-[#19363f] tracking-[-1.36px] max-sm:tracking-[-1px]">
                 39 Países
               </p>
-              <p className="font-normal text-[16px] leading-[24px] text-[rgba(25,54,63,0.7)] tracking-[-0.32px]">
+              <p className="font-normal text-[16px] max-md:text-[14px] max-sm:text-[13px] leading-6 max-sm:leading-5 text-[rgba(25,54,63,0.7)] tracking-[-0.32px] max-sm:tracking-[-0.26px]">
                 Retiradas en moneda local{" "}
               </p>
             </div>
-            <div className="flex flex-col gap-[16px] items-start ">
-              <p className="font-medium text-[28px] leading-[normal] text-[#19363f] tracking-[-1.36px]">
+            <div className="flex flex-col gap-[16px] items-start max-sm:w-full">
+              <p className="font-medium text-[28px] max-lg:text-[24px] max-md:text-[22px] max-sm:text-[20px] leading-[normal] text-[#19363f] tracking-[-1.36px] max-sm:tracking-[-1px]">
                 {/* +10 Comunididades */}
-                +55 Founders
+                +55 NFT Founders
               </p>
-              <p className="font-normal text-[16px] leading-[24px] text-[rgba(25,54,63,0.7)] tracking-[-0.32px]">
-                creciendo con founder de todo el mundo
+              <p className="font-normal text-[16px] max-md:text-[14px] max-sm:text-[13px] leading-6 max-sm:leading-5 text-[rgba(25,54,63,0.7)] tracking-[-0.32px] max-sm:tracking-[-0.26px]">
+                creciendo con founders de todo el mundo
                 {/* Hyxora por todo el mundo, con más de 50 NFT-Founder */}
               </p>
             </div>
